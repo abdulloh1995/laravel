7e8979a05331fcd222bbdf663aa89e665c011eb4
Abdulloh <developer.sobirov4448@gmail.com>
2022-02-16 20:57:18 +0500

    lesson 5 complete

--- my_blog/app/Http/Controllers/ContactController.php
+++ my_blog/app/Http/Controllers/ContactController.php
@@ -4,15 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\ContactRequest;
+use  App\Models\Contact;
 
 class ContactController extends Controller
 {
     public function submit(ContactRequest $req)
     {
-        // dd($req);
-        // $valid = $req->validate([
-        //     'comment' => 'required|min:5|max:50',
-        //     'message' => 'required|min:5|max:50'
-        // ]);
+        $contact = new Contact();
+        $contact->name = $req->input('name');
+        $contact->email = $req->input('email');
+        $contact->comment = $req->input('comment');
+        $contact->message = $req->input('message');
+
+        $contact->save();
+
+        return redirect()->route('contact')->with('success', 'Add to date base!!!');
     }
 }
